776c0580c59ff875515daaebd5386579cbe0261a
Jochen Parmentier <jochen.parmentier@external.atlascopco.com>
2023-07-26 12:47:29 +0200

    Adding explicit LOCATIONS list of Python filepaths to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,17 @@
 from nox_poetry import session, Session
 
 
+LOCATIONS = [
+    "tests",
+    "trickster",
+    "app.py",
+    "cli.py",
+    "gunicorn.conf.py",
+    "noxfile.py",
+    "setup.py",
+]
+
+
 @session(python=["3.8"])
 def test(session: Session) -> None:
     session.install(".", "pytest", "pytest-mock")
@@ -9,12 +20,14 @@ def test(session: Session) -> None:
 
 @session
 def lint(session: Session) -> None:
+    args = session.posargs or LOCATIONS
     session.install("flake8")
     # session.run("flake8", "--import-order-style", "google")
-    session.run("flake8")
+    session.run("flake8", *args)
 
 
 @session
 def black(session: Session) -> None:
+    args = session.posargs or LOCATIONS
     session.install("black")
-    session.run("black", ".")
+    session.run("black", *args)
